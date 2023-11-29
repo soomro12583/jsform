a22fdcf095ad06f724ff39fd4862d4cb03ceebac
soomro12583 <nadeemkhanasf909@gmail.com>
2023-11-29 22:57:17 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,5 @@
+var name = prompt("Please enter your name","Soomro");
+var phone = prompt("Please enter your Phone Number","03123432622");
+var email = prompt("Please enter your email", "nadeemkhan@gmail.com");
+var pass = prompt("Please enter Password","XXXXXXXXXXXXXXXXX");
+var con_pass = prompt("Please conform Password","XXXXXXXXXXXXXXXXX");
