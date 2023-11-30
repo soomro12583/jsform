a659527f9b32a08e265a0eadedd6cc0361e0d1f2
soomro12583 <nadeemkhanasf909@gmail.com>
2023-11-30 18:56:53 +0500

    practice

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 var name = prompt("Please enter your name","Soomro");
+var warning = ("you have not enterd name");
 var phone = prompt("Please enter your Phone Number","03123432622");
 var email = prompt("Please enter your email", "nadeemkhan@gmail.com");
 var pass = prompt("Please enter Password","XXXXXXXXXXXXXXXXX");
 var con_pass = prompt("Please conform Password","XXXXXXXXXXXXXXXXX");
+// for user name 
+if (name == ""|| name == " "){
+}else{alert=(warning);
+}
